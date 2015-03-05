5d2a436f5ea013d5708d8ad820c8de238e033401
Andrew Wen <awen@zendesk.com>
2015-03-05 09:41:43 -0800

    Added pods

---
+++ zentivity/zentivity-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "BDBOAuth1RequestOperationManager.h"
+#import "UIImageView+AFNetworking.h"
+#import "JGProgressHUD.h"
+#import "NSDictionary+BDBOAuth1Manager.h"
